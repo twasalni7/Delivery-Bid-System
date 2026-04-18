0fadad95b7ea37965b66e8f9aef93ce04c3746b3
twasalni7 <twasalni7@gmail.com>
2026-04-18 09:54:35 +0300

    Replace pino-http import with console logger

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -2,7 +2,7 @@ import express, { type Express } from "express";
 import cors from "cors";
 import session from "express-session";
 import connectPgSimple from "connect-pg-simple";
-import pinoHttp = require("pino-http");
+const logger = console
 import router from "./routes";
 import { logger } from "./lib/logger";
 import { pool } from "@workspace/db";
