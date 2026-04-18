a3eb8f91838e29179f32f6dcdb4ab513441b8d1d
twasalni7 <twasalni7@gmail.com>
2026-04-18 09:42:48 +0300

    Change import syntax for pino-http module

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -2,7 +2,7 @@ import express, { type Express } from "express";
 import cors from "cors";
 import session from "express-session";
 import connectPgSimple from "connect-pg-simple";
-import * as pinoHttp from "pino-http"; 
+import pinoHttp = require("pino-http");
 import router from "./routes";
 import { logger } from "./lib/logger";
 import { pool } from "@workspace/db";
