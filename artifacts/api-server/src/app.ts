5b4af0036d7196649d03f1cdec3a55d4d39a833b
twasalni7 <twasalni7@gmail.com>
2026-04-18 09:30:25 +0300

    Change import style for pino-http and update serializers

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -2,7 +2,7 @@ import express, { type Express } from "express";
 import cors from "cors";
 import session from "express-session";
 import connectPgSimple from "connect-pg-simple";
-import pinoHttp from "pino-http";
+import * as pinoHttp from "pino-http"; 
 import router from "./routes";
 import { logger } from "./lib/logger";
 import { pool } from "@workspace/db";
@@ -23,14 +23,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) { 
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) { 
         return {
           statusCode: res.statusCode,
         };
