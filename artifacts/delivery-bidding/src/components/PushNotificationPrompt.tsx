73b53da7de356ccffd97b6480dc64284a327b206
twasalni7 <twasalni7@gmail.com>
2026-05-09 05:08:23 +0300

    Update PushNotificationPrompt.tsx

--- artifacts/delivery-bidding/src/components/PushNotificationPrompt.tsx
+++ artifacts/delivery-bidding/src/components/PushNotificationPrompt.tsx
@@ -1,5 +1,5 @@
 import { Loader2 } from "lucide-react";
-import { usePushNotifications } from "from "@/hooks/use-push-notifications";
+import { usePushNotifications } from "@/hooks/use-push-notifications";
 
 /**
  * PushNotificationPrompt
@@ -42,3 +42,4 @@ export function PushNotificationPrompt() {
     </div>
   );
 }
+ 
